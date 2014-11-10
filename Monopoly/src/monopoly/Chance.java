01e0c89828471f59afa1d90d392dac56b32a0309
avinesse <avinesse@pc-b13-10.eleves.ecn>
2014-11-10 09:13:42 +0100

    Ajout de la classe Chance

---
+++ Monopoly/src/monopoly/Chance.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package monopoly;
+
+/**
+ *
+ * @author avinesse
+ */
+public class Chance extends Pioche {
+    
+}
