ac0e1041a217b920fe5e4c620e62a2eabf110abf
tlaurent <tlaurent@pc-b13-05.eleves.ecn>
2014-11-10 09:25:36 +0100

    Complétion de la classe Gare

--- Monopoly/src/monopoly/Gare.java
+++ Monopoly/src/monopoly/Gare.java
@@ -8,6 +8,19 @@ package monopoly;
  *
  * @author rlebail
  */
-public class Gare {
+public class Gare extends CaseAchetable {
     
+    private final int COEFGARE = 492;
+    
+    public Gare() {
+        super();
+    }
+    
+    public Gare(int prix) {
+        super(prix);
+    }
+    
+    public int calculLoyer() {
+        return (this.getProprietaire().getPlateau().nbGares(this.getProprietaire()) * COEFGARE);
+    }
 }
