c10ce671c4ccfbfa797716e38bcb9fe333404680
avinesse <avinesse@pc-b13-10.eleves.ecn>
2014-11-10 09:45:26 +0100

    Effets CarteChance

--- Monopoly/src/monopoly/CarteChance.java
+++ Monopoly/src/monopoly/CarteChance.java
@@ -18,15 +18,23 @@ public class CarteChance {
     
     
     
-    CarteChance(String t, String d) {
+    CarteChance(String t, String d, int a, int pos, boolean pri) {
         titre = t;
         description = d;
+        position = pos;
+        argent = a;
+        prison = pri;
         
     }
     
     void effet(Joueur, PlateauJeu) {
         
+        Joueur.fortune = Joueur.fortune+argent;
+        Joueur.position = Joueur.position+position;
         
+        if (prison) {
+            Joueur.position = 10;
+        }
         
     }
     
