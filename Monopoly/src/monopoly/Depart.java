5d68cd69cfab892abdaf45421bb2e379e1404e27
Акатош <@130.66.205.44>
2014-11-10 10:12:10 +0100

    case depart donne de la thune

--- Monopoly/src/monopoly/Depart.java
+++ Monopoly/src/monopoly/Depart.java
@@ -16,14 +16,13 @@ public class Depart extends Bonus{
         super("Depart",0);
     }
     /**
-     * Lance l'action de la case (donner 200€)
+     * Lance l'action de la case (donner 200€ au joueur)
      * @param joueur 
      */
     public void action(Joueur joueur){
         //Donne 200€ au joueur
         //Il aura également 200€ en passant par cette case (géré dans plateau)
         //S'il s'arrête pile sur cette case il recevra bien le double de 200€ (400€)
-        
-        
+        joueur.setFortune(joueur.getFortune()+200);
     }
 }
